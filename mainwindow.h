9b4fcd3bdcb62f5e342ae4f3acd7cbaff7b09957
toumasayasu <1543870401@qq.com>
2021-12-25 00:04:05 +0800

    第一版本完全版，实现了和参考示例一样的功能。

--- mainwindow.h
+++ mainwindow.h
@@ -2,20 +2,29 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
+#include <QFileDialog>
+#include "comdef.h"
+namespace Ui {
+class MainWindow;
+}
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
 private:
     Ui::MainWindow *ui;
+public slots:
+    void setFillStyle(QPen &qp, QBrush &qb);
+    void setLineStyle(QPen &qp, LINE_HEAD_TYPE &startLineHeadType, LINE_HEAD_TYPE &endLineHeadTypeb);
+    void disableStylePanel();
+    void sendSelChartLineColor();
+    void sendSelChartFillColor();
+    void changeWindowTitle(const QString &);
 };
+
 #endif // MAINWINDOW_H
