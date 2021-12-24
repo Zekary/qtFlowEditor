47e2f4470a1c5ef8c57e13a8f72ac7b17c58c2ee
toumasayasu <1543870401@qq.com>
2021-12-24 22:23:43 +0800

    实现了初步的ui界面设计

---
+++ flowchart.cpp
@@ -0,0 +1,6 @@
+#include "flowchart.h"
+
+flowchart::flowchart()
+{
+
+}
---
+++ flowchart.h
@@ -0,0 +1,11 @@
+#ifndef FLOWCHART_H
+#define FLOWCHART_H
+
+
+class flowchart
+{
+public:
+    flowchart();
+};
+
+#endif // FLOWCHART_H
